6afc09c4cfa50efd5af90a964828f9e714f32863
Dinakrushna Mohanta <159710872+Dinakrushna7077@users.noreply.github.com>
2026-07-30 00:00:59 +0530

    All required controller initialized

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CanteenManagement_2._0.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CanteenManagement_2._0.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
