d72ab0fe6e0b61640d65a40ff4c8659baf44b7bb
Dinakrushna Mohanta <159710872+Dinakrushna7077@users.noreply.github.com>
2026-07-30 12:40:57 +0530

    DapperContext Initialized

---
+++ Data/DapperContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace CanteenManagement_2._0.Data
+{
+    public class DapperContext
+    {
+        IConfiguration config;
+        public DapperContext(IConfiguration _config)
+        {
+            config= _config;
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
