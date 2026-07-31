172cf785a6b03beb3cef95baace9a1292eb50d83
Dinakrushna Mohanta <159710872+Dinakrushna7077@users.noreply.github.com>
2026-07-31 17:52:21 +0530

    Login page design completed

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,9 +2,11 @@
 
 namespace CanteenManagement_2._0.Controllers
 {
+    [Route("canteen")]
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        [HttpGet("login")]
+        public IActionResult Login()
         {
             return View();
         }
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CanteenManagement_2._0.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
