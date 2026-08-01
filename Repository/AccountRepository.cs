0e9805dc6990dbb0082f3d3e0fac3d5a0e36ba0d
Dinakrushna Mohanta <159710872+Dinakrushna7077@users.noreply.github.com>
2026-08-01 17:55:14 +0530

    Get User implemented in AccountRepository

---
+++ Repository/AccountRepository.cs
@@ -0,0 +1,25 @@
+﻿using CanteenManagement_2._0.Data;
+using CanteenManagement_2._0.Models;
+using CanteenManagement_2._0.Repository.Interfaces;
+using Dapper;
+using System.Text.RegularExpressions;
+using System.Data;
+
+namespace CanteenManagement_2._0.Repository
+{
+    public class AccountRepository:IAccountRepository
+    {
+        public readonly DapperContext db;
+        public AccountRepository(DapperContext db)
+        {
+            this.db = db;
+        }
+        public async Task<User> GetUserById(string uid)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@action", "GetUser");
+            param.Add("@uid", uid);
+            return (await db.GetConnection().QueryAsync<User>("ProcUserLogin", param, commandType: CommandType.StoredProcedure)).FirstOrDefault();
+        }
+    }
+}
