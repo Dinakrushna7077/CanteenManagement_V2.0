1a98c6a48510fb21eca7b97e558d34b1ebc9e26a
Dinakrushna Mohanta <159710872+Dinakrushna7077@users.noreply.github.com>
2026-08-01 17:56:27 +0530

    GetUser Definition

---
+++ Repository/Interfaces/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using CanteenManagement_2._0.Models;
+
+namespace CanteenManagement_2._0.Repository.Interfaces
+{
+    public interface IAccountRepository
+    {
+        Task<User> GetUserById(string uid);
+        
+    }
+}
