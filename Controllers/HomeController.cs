3608076efa2683cbfbd34c189ee21bd7933a714c
Dinakrushna Mohanta <159710872+Dinakrushna7077@users.noreply.github.com>
2026-08-05 11:23:50 +0530

    Public Query handled using ajax

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CanteenManagement_2._0.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CanteenManagement_2._0.Controllers
 {
@@ -7,6 +8,11 @@ namespace CanteenManagement_2._0.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> SubmitQuery(PublicQuery qry)
+        {
+            //Service call to save the query in the database
+            return await Task.FromResult<IActionResult>(Json(new {success = true, message = "Query submitted successfully!"}));
         }
     }
 }
