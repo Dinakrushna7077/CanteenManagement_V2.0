057eeadc805e0b4656abd8d9a781c6a9117266a0
Dinakrushna Mohanta <159710872+Dinakrushna7077@users.noreply.github.com>
2026-08-16 18:14:43 +0530

    New customer functionality initialized

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,10 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CanteenManagement_2._0.Models;
+using CanteenManagement_2._0.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CanteenManagement_2._0.Controllers
 {
     [Route("admin")]
     public class AdminController : Controller
     {
-        
+        private readonly IAdminService service;
+        public AdminController(IAdminService _service)
+        {
+            service= _service;
+        }
+        [HttpGet("add-customer")]
+        public IActionResult NewCustomer() => PartialView("_NewCustomer");
+        [HttpPost("add-customer")]
+        public async Task<IActionResult> NewCustomer(Customer cust)
+        {
+            ResponseViewModel<int> response = await service.NewCustomerAsync(cust);
+            if(response.Success)
+            {
+                return PartialView(response);
+            }
+            return PartialView("_NewCustomer",response);
+        }
     }
 }
